b5dab47eb48d352203381ec99f641a4cfece0671
Leonid Nikolaev <nikonor@nikonor.ru>
2012-01-26 11:37:39 +0400

    разнесли все по функциям-файлам разным 	new file:   user.py 	modified:   main.py 	modified:   st/index.js 	modified:   st/style.css 	modified:   templates/index.html

---
+++ user.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+class User():  
+    def __init__(self,id):
+
+        self.data = {'id':id,
+                     'name':'Иван Петрович Сидоров',
+                     "contacts":{
+                         "Age":29,
+                         "E-Mail":"ivan@sidorov.ru",
+                         "Twitter":"@sidorov",
+                         "Cell Phone":"+7-916-555-55-55",
+                         "Work Phone":"+7-495-555-55-55",
+                         "Home Phone":"+7-496-555-55-55",
+                     },
+                     "rels":{
+                         "Жена":{
+                             "id":2,
+                             "name":"Мария Сергеевна Сидорова"
+                         },
+                         "Сын":{
+                             "id":3,
+                             "name":"Андрей Иванович Сидоров"
+                         },
+
+                     }}
+        # print ("fname=%s" % self.data['fname']);                     
+    
+    def get(self,k):
+        return self.data[k]
+
+    def getall(self):
+        return self.data
